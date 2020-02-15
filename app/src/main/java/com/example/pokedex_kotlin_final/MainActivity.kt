a12ea84fd15d38165bd214c4be395bf5a4751d81
thanut <thanutza_dmex@gmail.com>
2020-02-15 17:30:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pokedex_kotlin_final/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokedex_kotlin_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
